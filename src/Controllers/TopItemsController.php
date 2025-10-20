7e2ba65aa06a71beab60ca14841094d8bbb2bcce
herrsaribas <ahmet.saribas@hafenx.com>
2025-10-20 16:37:21 +0200

    second changed

--- src/Controllers/TopItemsController.php
+++ src/Controllers/TopItemsController.php
@@ -4,13 +4,13 @@ namespace TopItems\Controllers;
 
 use Plenty\Plugin\Controller;
 use Plenty\Plugin\Templates\Twig;
-use Plenty\Modules\Item\DataLayer\Contracts\ItemDataLayerRepositoryContract;
-
+use Plenty\Modules\Item\DataLayer\Contracts\ItemDataLayerRepositoryContract; 
+ 
 class TopItemsController extends Controller
 {
     public function showTopItems(Twig $twig, ItemDataLayerRepositoryContract $itemRepository):string
     {
-        $itemColumns = [
+        $itemColumns = [ 
             'itemDescription' => [
                 'name1',
                 'description'
@@ -26,38 +26,30 @@ class TopItemsController extends Controller
                 'cleanImageName'
             ]
         ];
-
-        $itemFilter = [
+ 
+        $itemFilter = [ 
             'itemBase.isStoreSpecial' => [
-                'shopAction' => [1]
+                'shopAction' => [1] 
             ]
         ];
-
-        $itemParams = [
+ 
+        $itemParams = [ 
             'language' => 'de'
         ];
-
+ 
         $resultItems = $itemRepository
-            ->search($itemColumns, $itemFilter, $itemParams);
-
-        $items = array();
+            ->search($itemColumns, $itemFilter, $itemParams); 
+ 
+        $items = array(); 
         foreach ($resultItems as $item)
         {
-            // Resim URL'lerini düzelt
-            if (isset($item['variationImageList']) && is_array($item['variationImageList'])) {
-                foreach ($item['variationImageList'] as &$image) {
-                    if (isset($image['path']) && !empty($image['path'])) {
-                        $image['path'] = 'https://558vh82o1qhe.c01-14.plentymarkets.com' . $image['path'];
-                    }
-                }
-            }
             $items[] = $item;
         }
         $templateData = array(
             'resultCount' => $resultItems->count(),
             'currentItems' => $items
         );
-
-        return $twig->render('TopItems::content.TopItems', $templateData);
+ 
+        return $twig->render('TopItems::content.TopItems', $templateData); 
     }
 }
